83f60303a5f10ad7961432e703cd2d7005c5efc1
Viktor M <mbrhard.devops@gmail.com>
2026-06-11 11:30:19 +0000

    Add getReply method with hunter word

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+        @Test
+        public void testGetReplyContainsHunter() {
+                Welcomer welcomer = new Welcomer();
+                assertTrue(welcomer.getReply().contains("hunter"));
+        }
 }
